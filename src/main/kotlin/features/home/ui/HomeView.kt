8eadedcaf64f8192bb1b477f8cbb0049cfa37274
CeH9 <a.tahmazian@iwis.io>
2021-04-24 22:52:11 +0300

    Add demo scrollbars

--- src/main/kotlin/features/home/ui/HomeView.kt
+++ src/main/kotlin/features/home/ui/HomeView.kt
@@ -1,12 +1,15 @@
 package features.home.ui
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.*
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import core.CompositionKeys
 import features.home.ui.components.Editor
@@ -33,15 +36,36 @@ fun HomeView(viewModel: HomeViewModel) {
 @Composable
 private fun MainContent(state: HomeViewModel.State) {
     println("Visit MainContent")
-    Column(
-        modifier = Modifier.fillMaxSize().padding(16.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        val viewModel = CompositionKeys.ViewModel.current
+    val stateVertical = rememberScrollState(0)
+    Box {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+                .verticalScroll(stateVertical),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            val viewModel = CompositionKeys.ViewModel.current
 
-        Node(
-            item = state.root,
-            onEdit = { viewModel.onEditNodeClicked(state.root, null) },
+            Node(
+                item = state.root,
+                onEdit = { viewModel.onEditNodeClicked(state.root, null) },
+            )
+        }
+
+        VerticalScrollbar(
+            adapter = rememberScrollbarAdapter(stateVertical),
+            style = ScrollbarStyleAmbient.current.copy(
+//                unhoverColor = MaterialTheme.colors.primary,
+//                hoverColor = MaterialTheme.colors.primary.copy(alpha = 0.5f),
+                unhoverColor = Color.Green,
+                hoverColor = Color.Red,
+            ),
+            modifier = Modifier
+                .align(Alignment.CenterEnd)
+                .fillMaxHeight()
+//                .background(color = MaterialTheme.colors.primary),
+                .background(color = Color.Yellow),
         )
     }
 }
