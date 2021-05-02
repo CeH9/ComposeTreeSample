dddc89a61ceb71fe6be563df0642766c0e08a322
CeH9 <a.tahmazian@iwis.io>
2021-05-03 02:45:20 +0300

    Bump compose version

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.4.32"
-    id("org.jetbrains.compose") version "0.4.0-build184"
+    id("org.jetbrains.compose") version "0.4.0-build185"
 }
 
 group = "com.ceh9"
