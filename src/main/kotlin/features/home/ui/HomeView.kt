5c53681870b285195c08dfe8a23ee8bf68098bd4
CeH9 <a.tahmazian@iwis.io>
2021-05-03 02:45:40 +0300

    Add vertical scroll

--- src/main/kotlin/features/home/ui/HomeView.kt
+++ src/main/kotlin/features/home/ui/HomeView.kt
@@ -3,6 +3,8 @@ package features.home.ui
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
@@ -48,7 +50,10 @@ private fun MainContent(state: HomeViewModel.State) {
             Text("Job's Done!")
         }
         Column(
-            modifier = Modifier.fillMaxSize().padding(16.dp),
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+                .verticalScroll(rememberScrollState()),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Node(
